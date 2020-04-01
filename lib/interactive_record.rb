f17bc5a643bac259c5fa9f59bc4da77d0bf772c8
Harley McDowell <hmcdowell95@gmail.com>
2020-04-01 19:21:45 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -54,7 +54,8 @@ class InteractiveRecord
   end
   
   def self.find_by(k: v)
-    sql = "SELECT * FROM #{self.table_name} WHERE "
+    sql = "SELECT * FROM #{self.table_name} WHERE ? = ?"
+    DB[:conn].execute(sql, )
   end
 
 end
